33139c7dc77a88752d0ef7b9d3a82bce90b641bb
rarepeepo <188213000+rarepeepo@users.noreply.github.com>
2024-11-26 11:48:17 +0100

    Refactor IconButtonWidget
    Make IconButtonWidget extend from ButtonWidget rather than from
    TexturedButtonWidget to minimize the changes required for backporting to
    older versions of Minecraft.

--- common/src/main/java/rare/peepo/client/gui/IconButtonWidget.java
+++ common/src/main/java/rare/peepo/client/gui/IconButtonWidget.java
@@ -3,31 +3,32 @@ package rare.peepo.client.gui;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.ButtonTextures;
 import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.TexturedButtonWidget;
+import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import rare.peepo.BetterUX;
 
 @Environment(EnvType.CLIENT)
-public class IconButtonWidget extends TexturedButtonWidget {
+public class IconButtonWidget extends ButtonWidget {
     public static final int Size = 10;
+    final Identifier texture_1;
+    final Identifier texture_2;
 
     public IconButtonWidget(int x, int y, String name, ButtonWidget.PressAction pressAction) {
-        super(x, y, Size, Size,
-                new ButtonTextures(
-                        new Identifier(BetterUX.ID, name),
-                        new Identifier(BetterUX.ID, name + "_hover")),
-                pressAction);
+        super(x, y, Size, Size, ScreenTexts.EMPTY, pressAction, DEFAULT_NARRATION_SUPPLIER);
+        
+        texture_1 = new Identifier(BetterUX.ID, "textures/gui/" + name + ".png");
+        texture_2 = new Identifier(BetterUX.ID, "textures/gui/" + name + "_hover.png");
+        
         var s = Text.translatable(BetterUX.ID + "." + name + "Button.tooltip");
         this.setTooltip(Tooltip.of(s));
     }
 
     @Override
     public void renderWidget(DrawContext context, int mouseX, int mouseY, float delta) {
-        Identifier identifier = this.textures.get(this.isNarratable(), this.isHovered());
-        context.drawGuiTexture(identifier, this.getX(), this.getY(), this.width, this.height);
+        context.drawTexture(this.isHovered() ? texture_2 : texture_1, this.getX(), this.getY(),
+                0, 0, 0, this.width, this.height, this.width, this.height);
     }
 }
